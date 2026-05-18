c5ff7a86850e990c5f81fd0b3d97108c6d73e6b7
dheflo <florentin.dhenau@gmail.com>
2026-05-18 11:42:12 +0200

    Added rotation movement slider and input map

---
+++ _/scenes/inputs_ks4036_relay.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var input_map_rotate_left : String = "rotate_left"
+@export var input_map_rotate_right : String = "rotate_right"
+@export var input_map_move_forward : String = "move_forward"
+@export var input_map_move_back : String = "move_back"
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
--- _/scenes/ks4036_movement.gd
+++ _/scenes/ks4036_movement.gd
@@ -1,14 +1,26 @@
 extends Node
 
 @export var character : CharacterBody3D
+@export var max_speed_forward_in_mps : float = 2.5
+@export var max_rotation_in_degrees : float = 90
+@export_range(-1,1,0.01) var joystick_forward_intensity : float = 0
+@export_range(-1,1,0.01) var joystick_rotation_intensity : float = 0
+
 
 func _ready() -> void:
 	if character == null:
 		push_warning("Where is my f*king characterbody3D ?")
 
+func set_joystick_forward(percentage : float) :
+	joystick_forward_intensity = percentage
+
+func set_joystick_rotation(percentage : float) :
+	joystick_rotation_intensity = percentage
+
 func _process(delta: float) -> void:
 	var position : Vector3 = character.global_position
 	var direction_forward : Vector3 = -character.basis.z
 	direction_forward.y = 0
-	position = position + direction_forward * delta
+	position = position + direction_forward * delta * max_speed_forward_in_mps * joystick_forward_intensity
 	character.global_position = position
+	character.rotate_y(deg_to_rad(max_rotation_in_degrees) * delta * -joystick_rotation_intensity)
